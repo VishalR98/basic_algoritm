9b807817fd1083c6cda629577a30939361f27a8c
Vishal <rockstarbishal31@gmail.com>
2020-05-03 05:20:07 -0700

    added

---
+++ LargestAmongThreeNumbers.java
@@ -0,0 +1,16 @@
+
+        int a, b, c;
+                Scanner input2 = new Scanner(System.in);
+
+                a = input2.nextInt();
+                b = input2.nextInt();
+                c = input2.nextInt();
+                if (a>b)
+                if (a >c){
+                System.out.println("a is the largest number");
+                } else {
+                System.out.println("c is the largest number");
+                }else
+                if (b>c){
+                System.out.println("C is the greatest number");
+                }
---
+++ addTwoNumbers.java
@@ -0,0 +1,10 @@
+int num1 , num2 ,sum;
+        Scanner input = new Scanner(System.in); //Scanner is a class from util package
+        System.out.print("Enter a number: ");
+        num1=input.nextInt(); //Store input from user
+
+        System.out.print("Enter another number: ");
+        num2 = input.nextInt(); //Store input from user
+
+        sum = num1 + num2;
+        System.out.println("Sum = "+sum); 
